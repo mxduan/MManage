a44ecf42ff26126a9c21eae9732d5b7362ade0f3
Mike Duan <mduan@factset.com>
2016-03-27 22:02:36 -0700

    Initial commit - C++

---
+++ MManage/MManage/Main.cpp
@@ -0,0 +1,30 @@
+#pragma once
+#include <stdio.h>
+#include <iostream>
+
+/************************************************************************/
+/* 
+Megan has n number of sources of income. (need to be able to account for losses and new income sources)
+We have constant income on a schedule. Should account for possible adjustments
+We have m number of obligations on schedules
+We have k number of saving goals, each one on a different schedule
+
+we need to guarantee that the person will be able to pay for each bill obligation on schedule,
+have consistent discretionary income no matter what period in the schedule
+*/
+/************************************************************************/
+
+double CalculateSavings()
+{
+
+}
+
+double figureOutDisposableIncome()
+{
+
+}
+
+int main(int argv[], int args)
+{	
+	std::cout << "Hello World!";
+}
